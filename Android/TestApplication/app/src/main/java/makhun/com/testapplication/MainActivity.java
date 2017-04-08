cf1bf207f94260619550a27ec63eec19b5763437
MaximAlien <maxim.makhun@gmail.com>
2017-04-08 16:00:00 +0300

    Add secondary activity for more tests.

--- Android/TestApplication/app/src/main/java/makhun/com/testapplication/MainActivity.java
+++ Android/TestApplication/app/src/main/java/makhun/com/testapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package makhun.com.testapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
         testApplication.onActivityCreated(this, savedInstanceState);
 
         init();
+
+        Button openSecondaryActivityButton = (Button)findViewById(R.id.secondaryActivityButton);
+        openSecondaryActivityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondaryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public boolean fillFields(String firstname, String lastname) {
---
+++ Android/TestApplication/app/src/main/java/makhun/com/testapplication/SecondaryActivity.java
@@ -0,0 +1,13 @@
+package makhun.com.testapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SecondaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondary);
+    }
+}
